7de1e453d035e74c5c26a2ed86ff4a97823b484d
David Marquardt <davidmarquardt@me.com>
2017-06-22 16:01:22 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+
+function setup(){
+  createCanvas(1024, 1024);
+  rectMode(CENTER);
+}
+
+// function preload() {
+//   myFont = loadFont('https://github.com/google/fonts/blob/master/ofl/montserrat/Montserrat-Regular.ttf?raw=true');
+//   console.log(myFont);
+// } 
+
+// function getPhotos(){
+//   $.ajax({
+//     url: 'https://api.unsplash.com/photos/?client_id= ',
+//   }).done(function(response, textStatus, jqXHR){
+//     console.log(response);
+//   }).fail(function(jqXHR, status, errorThrown) {
+//     var response = $.parseJSON(jqXHR.responseText);
+//     console.log(response);
+//     throwErrorMessage(response, product);
+//   });
+// }
+
+function title(){
+  let coverName = "Cigarro e Solidão.";
+  push();
+  fill(255,255,255);
+  textSize(122);
+  textAlign(LEFT);
+  textFont('Montserrat-Bold');
+  text(coverName, 400, 400, 300, 600);
+  pop();
+}
+
+function particles (){
+  
+}
+
+function draw(){
+  background(35, 35, 35);
+  title();
+}
